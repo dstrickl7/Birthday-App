075dd2ce640c2c029970847efeae1aa4d29ebbc6
Danielle S <strickl6@illinois.edu>
2021-06-10 15:40:51 -0500

    styled person component

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../styles/Person.css';
+import person from '../styles/person1.jpg'
 
 function Person(props) {
     return (
         <div className="person-container">
-            <span className="photo-container">
-                <img src="" alt={props.name} className="photo" />
-            </span>
+            <div className="photo-container">
+                <img src={person} alt={props.name} className="photo" />
+            </div>
             <div>
                 <h3 className="name">{props.name}</h3>
-                <p className="age">{props.age}</p>
+                <p className="age">{props.age} years</p>
             </div>
         </div>
     )
